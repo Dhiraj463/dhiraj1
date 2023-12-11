5e6d13969ec1989abaafdf42c09f3e583fc6d380
Dhiraj463 <146825287+Dhiraj463@users.noreply.github.com>
2023-12-10 22:07:49 -0800

    Add files via upload

---
+++ dp.py
@@ -0,0 +1,21 @@
+# Program to perform input function
+
+secret_value=500
+definputfunction():
+    secret_return
+
+input1 = raw_input("Raw_input()"):
+    find secret_value
+
+if input1 == secret_value:
+    print "you guessed correct"
+else:
+    print "wrong answer"
+
+input2 = Input()("input_value"):
+    find secret value
+
+if input2 == secret_value:
+    print "you guessed correct"
+else:
+    print "wrong answer"
